befd02d29f6bce130d39e8bc0bec643ddf5c908d
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-04-15 03:41:58 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200415033807) do
+ActiveRecord::Schema.define(version: 20200415033650) do
 
   create_table "doctors", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "patients", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
